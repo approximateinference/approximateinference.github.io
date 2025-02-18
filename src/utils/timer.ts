7a166585d09baee3fca458ac30fdc20df5de45cc
Agustinus Kristiadi <agustinus@kristia.de>
2025-02-17 22:10:43 -0500

    Make countdown dynamic

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,13 +1,19 @@
 import { TZDate } from "@date-fns/tz";
 import { differenceInMilliseconds } from "date-fns";
 
-export function calculateTimeLeft(targetDate: TZDate): {
+export function calculateTimeLeft(
+  year: number,
+  month: number,
+  day: number,
+): {
   days: number;
   hours: number;
   minutes: number;
   seconds: number;
 } {
+  const targetDate = new TZDate(year, month - 1, day, "-12");
   const now = new TZDate(Date.now(), "-12"); // AoE is UTC-12
+
   const timeDifference = differenceInMilliseconds(targetDate, now);
 
   if (timeDifference <= 0) {
