8f1dc5ce608da6660bd980eb1cb94d40f3cce45d
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2026-02-02 16:45:22 -0500

    updated for ProbML 2026
    - Updated name to ProbML 2026
    - updated venue where applicable
    - added new members to organizer page
    - updated call for paper and mission statement on main page
    - commented out details to be confirmed (sponsors, speakers)

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,11 +4,11 @@ import "@fontsource/roboto-condensed";
 
 export const siteConfig: SiteConfig = {
   // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-  author: "AABI Organizers",
+  author: "ProbML Organizers",
   // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-  title: "AABI 2025",
+  title: "ProbML 2026",
   // Meta property used as the default description meta property
-  description: "Advances in Approximate Bayesian Inference",
+  description: "Symposium on Probabilistic Machine Learning",
   // HTML lang property, found in src/layouts/Base.astro L:18
   lang: "en-US",
   // Meta property, found in src/components/BaseHead.astro L:42
